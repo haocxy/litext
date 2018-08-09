f548e6cda5244ffc5cbb65d72c10b950bb6a439d
蔡心宇 <haocxy@qq.com>
2018-08-10 01:19:55 +0800

    修改内部数据类型

--- src/util/listeners.h
+++ src/util/listeners.h
@@ -2,14 +2,13 @@
 
 #include <vector>
 #include <functional>
-#include <cstdint>
 #include <memory>
 
 class RemovableListeners
 {
 public:
     virtual ~RemovableListeners() {}
-    virtual void remove(uintmax_t id) = 0;
+    virtual void remove(void * id) = 0;
 };
 
 // ¼àÌıÆ÷¾ä±ú
@@ -30,7 +29,7 @@ public:
         , m_listenerId(listenerHandler.m_listenerId)
     {
         listenerHandler.m_listeners = nullptr;
-        listenerHandler.m_listenerId = 0;
+        listenerHandler.m_listenerId = nullptr;
     }
 
     ListenerHandle & operator=(ListenerHandle && h)
@@ -39,26 +38,26 @@ public:
         h.m_listeners = nullptr;
 
         m_listenerId = h.m_listenerId;
-        h.m_listenerId = 0;
+        h.m_listenerId = nullptr;
 
         return *this;
     }
 
     ~ListenerHandle()
     {
-        if (m_listeners)
+        if (m_listeners && m_listenerId)
         {
             m_listeners->remove(m_listenerId);
             m_listeners = nullptr;
-            m_listenerId = 0;
+            m_listenerId = nullptr;
         }
     }
 
 private:
     std::shared_ptr<RemovableListeners> m_listeners;
-    uintmax_t m_listenerId;
+    void * m_listenerId;
 
-    ListenerHandle(std::shared_ptr<RemovableListeners> listeners, uintmax_t id) : m_listeners(listeners), m_listenerId(id) {}
+    ListenerHandle(std::shared_ptr<RemovableListeners> listeners, void * id) : m_listeners(listeners), m_listenerId(id) {}
 
     template <typename Fn>
     friend class Listeners;
@@ -70,18 +69,17 @@ class ListenersInner : public RemovableListeners
 public:
     virtual ~ListenersInner() {}
 
-    uintmax_t add(std::function<Fn> && f)
+    void * add(std::function<Fn> && f)
     {
         m_vec.push_back(std::move(f));
-        uintmax_t id = uintmax_t(&(m_vec.back()));
-        return id;
+        return &(m_vec.back());
     }
 
-    virtual void remove(uintmax_t id) override
+    virtual void remove(void * id) override
     {
         for (Vec::size_type i = 0; i < m_vec.size(); ++i)
         {
-            if (uintmax_t(&(m_vec[i])) == id)
+            if (&(m_vec[i]) == id)
             {
                 m_vec[i].swap(m_vec.back());
                 m_vec.pop_back();
@@ -109,7 +107,7 @@ class Listeners
 public:
     ListenerHandle add(std::function<Fn> && f)
     {
-        uintmax_t id = m_ptr->add(std::move(f));
+        void * id = m_ptr->add(std::move(f));
         return ListenerHandle(m_ptr, id);
     }
 
