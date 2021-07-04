041e855ca7a9e7d1c05b496cde32cf80c08dfa80
蔡心宇 <haocxy@qq.com>
2021-07-04 13:29:48 +0800

    a

--- src/core/uchar.h
+++ src/core/uchar.h
@@ -25,9 +25,11 @@ inline constexpr bool isLowSurrogate(T ch) {
 	return (ch & (~T(0b11'1111'1111))) == (T(0b1101'11) << T(10));
 }
 
+constexpr UChar U16SurrogatePairToUnicodeDelta = (UChar(0b1101'10) << UChar(20)) + (UChar(0b1101'11) << UChar(10)) - UChar(0x10000);
+
 template <typename T>
 inline constexpr UChar u16SurrogatePairToUnicode(T high, T low) {
-	return '%'; // TODO
+	return (UChar(high) << UChar(10)) + UChar(low) - U16SurrogatePairToUnicodeDelta;
 }
 
 }
