3b533b17839b82b566de9823f4ceb305165af25b
蔡心宇 <haocxy@qq.com>
2021-07-04 23:18:30 +0800

    a

--- src/gui/font_to_qfont.h
+++ src/gui/font_to_qfont.h
@@ -4,14 +4,14 @@
 #include <QFont>
 
 
-void fontToQFont(const Font &font, QFont &qfont)
+static void fontToQFont(const Font &font, QFont &qfont)
 {
 	qfont.setFamily(font.family().c_str());
 	qfont.setPointSize(font.size());
 	qfont.setBold(font.bold());
 }
 
-QFont fontToQFont(const Font &font)
+static QFont fontToQFont(const Font &font)
 {
 	QFont qfont;
 	fontToQFont(font, qfont);
