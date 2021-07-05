a46fb35ac061ee62af99200a848d3eba08fb2e90
蔡心宇 <haocxy@qq.com>
2021-07-05 16:52:45 +0800

    a

--- src/gui/view_loc.h
+++ src/gui/view_loc.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <bitset>
-
 #include "doc/doc_define.h"
 #include "view_define.h"
 
@@ -12,40 +10,33 @@ namespace gui
 
 class ViewLoc {
 public:
-    ViewLoc() { m_flag.set(flgNull); }
-    explicit ViewLoc(RowN row) : m_row(row), m_line(0) {}
-    ViewLoc(RowN row, LineN line) : m_row(row), m_line(line) {}
+    ViewLoc() {}
 
-    bool isNull() const { return m_flag.test(flgNull); }
-    bool atEnd() const { return m_flag.test(flgAtEnd); }
+    explicit ViewLoc(RowN row) : row_(row), line_(0) {}
 
-    RowN row() const { return m_row; }
-    void setRow(RowN row) { m_row = row; }
+    ViewLoc(RowN row, LineN line) : row_(row), line_(line) {}
 
-    LineN line() const { return m_line; }
-    void setLine(LineN line) { m_line = line; }
+    RowN row() const { return row_; }
+
+    void setRow(RowN row) { row_ = row; }
+
+    LineN line() const { return line_; }
+
+    void setLine(LineN line) { line_ = line; }
 
     bool operator==(const ViewLoc &b) const
     {
-        return m_row == b.m_row && m_line == b.m_line && m_flag == b.m_flag;
+        return row_ == b.row_ && line_ == b.line_;
     }
+
     bool operator!=(const ViewLoc &b) const
     {
         return !(*this == b);
     }
 
 private:
-    enum Flag {
-        flgNull,
-        flgAtEnd,
-
-        flgCnt,
-    };
-
-private:
-    RowN m_row = 0;
-    LineN m_line = 0;
-    std::bitset<flgCnt> m_flag;
+    RowN row_ = 0;
+    LineN line_ = 0;
 };
 
 
