e4801123338cb66c852dca474d0b5894853b9c03
蔡心宇 <haocxy@qq.com>
2021-07-16 23:14:39 +0800

    add MemBuff

---
+++ src/core/membuff.cpp
@@ -0,0 +1,94 @@
+#include "membuff.h"
+
+
+namespace mem
+{
+
+void MemBuff::resize(size_t len) {
+    ensureCapacity(len);
+    size_ = len;
+}
+
+size_t MemBuff::read(size_t offset, void *to, size_t len) const {
+    if (size_ <= offset) {
+        return 0;
+    }
+
+    const size_t readable = std::min(size_ - offset, len);
+    std::memcpy(to, beg_ + offset, readable);
+    return readable;
+}
+
+size_t MemBuff::remove(size_t offset, size_t len) {
+    if (size_ <= offset) {
+        return 0;
+    }
+
+    const size_t removable = std::min(size_ - offset, len);
+    const size_t endOffset = offset + removable;
+    if (endOffset < size_) {
+        std::memmove(beg_ + offset, beg_ + endOffset, size_ - endOffset);
+    }
+    size_ -= removable;
+    return removable;
+}
+
+size_t MemBuff::take(size_t offset, void *to, size_t len) {
+    if (size_ <= offset) {
+        return 0;
+    }
+
+    const size_t takable = std::min(size_ - offset, len);
+    const size_t endOffset = offset + takable;
+    std::memcpy(to, beg_ + offset, takable);
+    if (endOffset < size_) {
+        std::memmove(beg_ + offset, beg_ + endOffset, size_ - endOffset);
+    }
+    size_ -= takable;
+    return takable;
+}
+
+void MemBuff::write(size_t offset, const void *data, size_t len) {
+    ensureCapacity(offset + len);
+
+    // 若当前字节数小于指定偏移,先用0字节把这段空白初始化
+    if (size_ < offset) {
+        std::memset(beg_ + size_, 0, offset - size_);
+    }
+
+    // 拷贝到目标内存区
+    std::memcpy(beg_ + offset, data, len);
+
+    // 如果目标区结束位置超出了当前字节数，则更新当前字节数
+    size_ = std::max(size_, offset + len);
+}
+
+void MemBuff::append(const void *from, size_t len) {
+    const size_t newbytecount = size_ + len;
+    ensureCapacity(newbytecount);
+    std::memcpy(beg_ + size_, from, len);
+    size_ = newbytecount;
+}
+
+size_t MemBuff::CalcRealNewCapacity(size_t curCapacity, size_t newCapacity) {
+    constexpr size_t kInit = 256;
+    if (curCapacity != 0) {
+        size_t n = curCapacity;
+        while (n < newCapacity) {
+            n *= 2;
+        }
+        return n;
+    } else {
+        return kInit;
+    }
+}
+
+void MemBuff::realloc(size_t newcapacity) {
+    capacity_ = newcapacity;
+    byte *ptr = new byte[capacity_];
+    std::memcpy(ptr, beg_, size_);
+    delete beg_;
+    beg_ = ptr;
+}
+
+}
---
+++ src/core/membuff.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#include <algorithm>
+#include <cstring>
+
+
+namespace mem
+{
+
+class MemBuff
+{
+public:
+
+    MemBuff() {}
+
+    explicit MemBuff(size_t capacity) { reverse(capacity); }
+
+    MemBuff(const void *buff, size_t len) { write(buff, len); }
+
+    MemBuff(const MemBuff &b) { write(b.data(), b.size()); }
+
+    MemBuff(MemBuff &&b) noexcept : beg_(b.beg_), size_(b.size_), capacity_(b.capacity_) {
+        b.beg_ = nullptr;
+        b.size_ = 0;
+        b.capacity_ = 0;
+    }
+
+    MemBuff &operator=(const MemBuff &b) {
+        clear();
+        write(b.data(), b.size());
+        return *this;
+    }
+
+    MemBuff &operator=(MemBuff &&b) noexcept {
+        if (this != &b) {
+            delete[] beg_;
+            beg_ = b.beg_;
+            b.beg_ = nullptr;
+
+            size_ = b.size_;
+            b.size_ = 0;
+
+            capacity_ = b.capacity_;
+            b.capacity_ = 0;
+        }
+        return *this;
+    }
+
+    ~MemBuff() { delete[] beg_; }
+
+    void clear() { size_ = 0; }
+
+    const void *data() const { return beg_; }
+
+    void *data() { return beg_; }
+
+    size_t size() const { return size_; }
+
+    void resize(size_t len);
+
+    size_t read(size_t offset, void *to, size_t len) const;
+
+    size_t read(void *to, size_t len) const { return read(0, to, len); }
+
+    size_t remove(size_t offset, size_t len);
+
+    size_t popFront(size_t len) {
+        return remove(0, len);
+    }
+
+    size_t take(size_t offset, void *to, size_t len);
+
+    void write(size_t offset, const void *data, size_t len);
+
+    void write(const void *data, size_t len) { write(0, data, len); }
+
+    void reverse(size_t len) { ensureCapacity(len); }
+
+    void append(const void *from, size_t len);
+
+private:
+    using byte = unsigned char;
+
+    static_assert(sizeof(unsigned char) == 1);
+    static_assert(sizeof(byte) == 1);
+
+    static size_t CalcRealNewCapacity(
+        size_t curCapacity,
+        size_t newCapacity);
+
+    void ensureCapacity(size_t required) {
+        if (capacity_ >= required) {
+            return;
+        }
+        realloc(CalcRealNewCapacity(capacity_, required));
+    }
+
+    void realloc(size_t newcapacity);
+
+private:
+    byte *beg_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+};
+
+}
