e8ed7de603a5d744715d4cff7db1184bb2d34bec
蔡心宇 <haocxy@qq.com>
2021-07-18 00:22:17 +0800

    format file

--- src/doc/document_impl.cpp
+++ src/doc/document_impl.cpp
@@ -13,69 +13,69 @@ namespace doc::detail
 {
 
 DocumentImpl::DocumentImpl(const fs::path &path, Worker &ownerThread)
-	: path_(path)
-	, asyncComponents_(new AsyncComponents(path))
-	, db_(path.generic_string() + ".notesharp.db")
-	, ownerThread_(ownerThread)
+    : path_(path)
+    , asyncComponents_(new AsyncComponents(path))
+    , db_(path.generic_string() + ".notesharp.db")
+    , ownerThread_(ownerThread)
 {
-	LOGD << "DocumentImpl::DocumentImpl(...) for [" << path << "]";
+    LOGD << "DocumentImpl::DocumentImpl(...) for [" << path << "]";
 }
 
 static uintmax_t partSize() {
-	return SystemUtil::pageSize() * 1024;
+    return SystemUtil::pageSize() * 1024;
 }
 
 static void skipRowEndForAscii(std::ifstream &ifs, std::vector<unsigned char> &skipBuff) {
-	constexpr char eof = std::ifstream::traits_type::eof();
-	char ch = 0;
-	while ((ch = ifs.get()) != eof) {
-		skipBuff.push_back(ch);
-		if (ch == '\r') {
-			char next = 0;
-			if ((next = ifs.get()) != eof) {
-				if (next == '\n') {
-					skipBuff.push_back(next);
-					return;
-				} else {
-					ifs.unget();
-					continue;
-				}
-			} else {
-				return;
-			}
-		} else if (ch == '\n') {
-			return;
-		} else {
-			continue;
-		}
-	}
+    constexpr char eof = std::ifstream::traits_type::eof();
+    char ch = 0;
+    while ((ch = ifs.get()) != eof) {
+        skipBuff.push_back(ch);
+        if (ch == '\r') {
+            char next = 0;
+            if ((next = ifs.get()) != eof) {
+                if (next == '\n') {
+                    skipBuff.push_back(next);
+                    return;
+                } else {
+                    ifs.unget();
+                    continue;
+                }
+            } else {
+                return;
+            }
+        } else if (ch == '\n') {
+            return;
+        } else {
+            continue;
+        }
+    }
 }
 
 static void skipRowEndForUnknown(std::ifstream &ifs, std::vector<unsigned char> &skipBuff) {
-	skipRowEndForAscii(ifs, skipBuff);
+    skipRowEndForAscii(ifs, skipBuff);
 }
 
 // 把文件流ifs定位到换行字节组之后的下一个字节
 static void skipRowEnd(std::ifstream &ifs, Charset charset, std::vector<unsigned char> &skipBuff) {
-	switch (charset) {
-	case Charset::Ascii:
-	case Charset::UTF_8:
-	case Charset::GB18030:
-		skipRowEndForAscii(ifs, skipBuff);
-		return;
-	default:
-		skipRowEndForUnknown(ifs, skipBuff);
-		return;
-	}
+    switch (charset) {
+    case Charset::Ascii:
+    case Charset::UTF_8:
+    case Charset::GB18030:
+        skipRowEndForAscii(ifs, skipBuff);
+        return;
+    default:
+        skipRowEndForUnknown(ifs, skipBuff);
+        return;
+    }
 }
 
 static void moveFileStreamPosToAfterNewLine(Charset charset, std::ifstream &ifs, MemBuff &partBuff)
 {
-	std::vector<unsigned char> skipBuff;
-	skipRowEnd(ifs, charset, skipBuff);
-	if (!skipBuff.empty()) {
-		partBuff.append(skipBuff.data(), skipBuff.size());
-	}
+    std::vector<unsigned char> skipBuff;
+    skipRowEnd(ifs, charset, skipBuff);
+    if (!skipBuff.empty()) {
+        partBuff.append(skipBuff.data(), skipBuff.size());
+    }
 }
 
 void DocumentImpl::loadPart(AsyncComponents &comps, const MemBuff &data) const
@@ -85,58 +85,58 @@ void DocumentImpl::loadPart(AsyncComponents &comps, const MemBuff &data) const
 
 void DocumentImpl::loadDocument(AsyncComponents &comps) const
 {
-	static const char *title = "DocumentImpl::loadDocument() ";
+    static const char *title = "DocumentImpl::loadDocument() ";
 
-	ElapsedTime elapsedTime;
+    ElapsedTime elapsedTime;
 
-	LOGD << title << "start for file [" << path_ << "]";
+    LOGD << title << "start for file [" << path_ << "]";
 
-	std::ifstream &ifs = comps.ifs();
-	MemBuff &buff = comps.buff();
-	CharsetDetector &charsetDetector = comps.charsetDetector();
+    std::ifstream &ifs = comps.ifs();
+    MemBuff &buff = comps.buff();
+    CharsetDetector &charsetDetector = comps.charsetDetector();
 
-	const uintmax_t partLen = partSize();
+    const uintmax_t partLen = partSize();
 
-	uintmax_t partLenSum = 0;
-	
-	for (uintmax_t partIndex = 0; ifs; ++partIndex) {
+    uintmax_t partLenSum = 0;
 
-		buff.reverse(partLen);
-		ifs.read(reinterpret_cast<char *>(buff.data()), partLen);
-		const uintmax_t gcount = ifs.gcount();
-		buff.resize(gcount);
+    for (uintmax_t partIndex = 0; ifs; ++partIndex) {
 
-		charsetDetector.feed(buff.data(), gcount);
-		charsetDetector.end();
+        buff.reverse(partLen);
+        ifs.read(reinterpret_cast<char *>(buff.data()), partLen);
+        const uintmax_t gcount = ifs.gcount();
+        buff.resize(gcount);
 
-		const std::string scharset = charsetDetector.charset();
-		const Charset charset = CharsetUtil::strToCharset(scharset);
+        charsetDetector.feed(buff.data(), gcount);
+        charsetDetector.end();
 
-		moveFileStreamPosToAfterNewLine(charset, ifs, buff);
+        const std::string scharset = charsetDetector.charset();
+        const Charset charset = CharsetUtil::strToCharset(scharset);
 
-		loadPart(comps, buff);
+        moveFileStreamPosToAfterNewLine(charset, ifs, buff);
 
-		LOGD << title << " part(" << partIndex << ") gcount [" << gcount << "], part len: [" << buff.size() << "]";
+        loadPart(comps, buff);
 
-		partLenSum += buff.size();
+        LOGD << title << " part(" << partIndex << ") gcount [" << gcount << "], part len: [" << buff.size() << "]";
 
-		buff.clear();
-	}
+        partLenSum += buff.size();
 
-	LOGD << title << "end for file [" << path_ << "]";
-	LOGD << title << "part len sum : [" << partLenSum << "](" << ReadableSizeUtil::convert(partLenSum, 2) << ")";
-	LOGD << title << "time usage : " << elapsedTime.milliSec() << "ms";
+        buff.clear();
+    }
+
+    LOGD << title << "end for file [" << path_ << "]";
+    LOGD << title << "part len sum : [" << partLenSum << "](" << ReadableSizeUtil::convert(partLenSum, 2) << ")";
+    LOGD << title << "time usage : " << elapsedTime.milliSec() << "ms";
 }
 
 void DocumentImpl::asyncLoadDocument()
 {
-	auto self(shared_from_this());
-	std::async(std::launch::async, [this, self, comps = asyncComponents_] {
-		loadDocument(*comps);
-		ownerThread_.post([this, self, comps] {
-			asyncComponents_ = comps;
-		});
-	});
+    auto self(shared_from_this());
+    std::async(std::launch::async, [this, self, comps = asyncComponents_] {
+        loadDocument(*comps);
+        ownerThread_.post([this, self, comps] {
+            asyncComponents_ = comps;
+            });
+        });
 }
 
 DocumentImpl::~DocumentImpl()
@@ -146,32 +146,32 @@ DocumentImpl::~DocumentImpl()
 
 void DocumentImpl::start()
 {
-	asyncLoadDocument();
+    asyncLoadDocument();
 }
 
 void DocumentImpl::bind(DocumentListener &listener)
 {
-	listener_ = &listener;
+    listener_ = &listener;
 }
 
 void DocumentImpl::unbind()
 {
-	listener_ = nullptr;
+    listener_ = nullptr;
 }
 
 bool DocumentImpl::loaded() const
 {
-	return loaded_;
+    return loaded_;
 }
 
 RowN DocumentImpl::loadedRowCount() const
 {
-	return loadedRowCount_;
+    return loadedRowCount_;
 }
 
 Charset DocumentImpl::charset() const
 {
-	return charset_;
+    return charset_;
 }
 
 }
--- src/doc/document_impl.h
+++ src/doc/document_impl.h
@@ -18,146 +18,146 @@ namespace doc::detail
 class DocumentImpl : public std::enable_shared_from_this<DocumentImpl> {
 private:
 
-	using Db = sqlite3ns::Database;
+    using Db = sqlite3ns::Database;
 
-	// 需要异步处理的组件都放在这个类中
-	class AsyncComponents {
-	public:
-		AsyncComponents(const fs::path &filePath)
-			: ifs_(filePath, std::ios::binary) {}
+    // 需要异步处理的组件都放在这个类中
+    class AsyncComponents {
+    public:
+        AsyncComponents(const fs::path &filePath)
+            : ifs_(filePath, std::ios::binary) {}
 
-		std::ifstream &ifs() {
-			return ifs_;
-		}
+        std::ifstream &ifs() {
+            return ifs_;
+        }
 
-		MemBuff &buff() {
-			return buff_;
-		}
+        MemBuff &buff() {
+            return buff_;
+        }
 
-		CharsetDetector &charsetDetector() {
-			return charsetDetector_;
-		}
+        CharsetDetector &charsetDetector() {
+            return charsetDetector_;
+        }
 
-	private:
-		std::ifstream ifs_;
-		MemBuff buff_;
-		CharsetDetector charsetDetector_;
-	};
+    private:
+        std::ifstream ifs_;
+        MemBuff buff_;
+        CharsetDetector charsetDetector_;
+    };
 
-	// 用于把异步组件AsyncComponents的对象在不同的线程中移动，以确保只有一个线程能够处理这些组件。
-	// 这个类会在任何拷贝语义和移动语义时转移内部指针的所有，本质上是为了解决使用lambda时遇到的一些问题。
-	//
-	// 问：为什么不使用std::unique_ptr ？
-	// 答：std::unique_ptr 移动对象的能力基于对于移动语义的适配，但是目前发现lambda的捕获列表无法很好地支持移动语义
-	//
-	// 问：为什么不直接使用原始的指针 ？
-	// 答：在移动的过程中，如果其所在的函数对象丢失，则会导致对象丢失，不再被任何逻辑管理，进一步导致内存泄露及其它可能的问题
-	//
-	// 问：为什么不把这个类实现为模板类？
-	// 答：这里的处理方式是不得以而为之，是很不好的。
-	// 
-	class AsyncComponentsMovePointer {
-	public:
-		AsyncComponentsMovePointer() {}
+    // 用于把异步组件AsyncComponents的对象在不同的线程中移动，以确保只有一个线程能够处理这些组件。
+    // 这个类会在任何拷贝语义和移动语义时转移内部指针的所有，本质上是为了解决使用lambda时遇到的一些问题。
+    //
+    // 问：为什么不使用std::unique_ptr ？
+    // 答：std::unique_ptr 移动对象的能力基于对于移动语义的适配，但是目前发现lambda的捕获列表无法很好地支持移动语义
+    //
+    // 问：为什么不直接使用原始的指针 ？
+    // 答：在移动的过程中，如果其所在的函数对象丢失，则会导致对象丢失，不再被任何逻辑管理，进一步导致内存泄露及其它可能的问题
+    //
+    // 问：为什么不把这个类实现为模板类？
+    // 答：这里的处理方式是不得以而为之，是很不好的。
+    // 
+    class AsyncComponentsMovePointer {
+    public:
+        AsyncComponentsMovePointer() {}
 
-		AsyncComponentsMovePointer(AsyncComponents *ptr) : ptr_(ptr) {}
+        AsyncComponentsMovePointer(AsyncComponents *ptr) : ptr_(ptr) {}
 
-		AsyncComponentsMovePointer(const AsyncComponentsMovePointer &other) {
-			if (this != &other && ptr_ != other.ptr_) {
-				ptr_ = other.ptr_;
-				mute(other).ptr_ = nullptr;
-			}
-		}
+        AsyncComponentsMovePointer(const AsyncComponentsMovePointer &other) {
+            if (this != &other && ptr_ != other.ptr_) {
+                ptr_ = other.ptr_;
+                mute(other).ptr_ = nullptr;
+            }
+        }
 
-		AsyncComponentsMovePointer(AsyncComponentsMovePointer &&other) noexcept {
-			if (this != &other && ptr_ != other.ptr_) {
-				ptr_ = other.ptr_;
-				other.ptr_ = nullptr;
-			}
-		}
+        AsyncComponentsMovePointer(AsyncComponentsMovePointer &&other) noexcept {
+            if (this != &other && ptr_ != other.ptr_) {
+                ptr_ = other.ptr_;
+                other.ptr_ = nullptr;
+            }
+        }
 
-		~AsyncComponentsMovePointer() {
-			delete ptr_;
-			ptr_ = nullptr;
-		}
+        ~AsyncComponentsMovePointer() {
+            delete ptr_;
+            ptr_ = nullptr;
+        }
 
-		AsyncComponentsMovePointer &operator=(const AsyncComponentsMovePointer &other) {
-			if (this != &other && ptr_ != other.ptr_) {
-				delete ptr_;
-				ptr_ = other.ptr_;
-				mute(other).ptr_ = nullptr;
-			}
-			return *this;
-		}
+        AsyncComponentsMovePointer &operator=(const AsyncComponentsMovePointer &other) {
+            if (this != &other && ptr_ != other.ptr_) {
+                delete ptr_;
+                ptr_ = other.ptr_;
+                mute(other).ptr_ = nullptr;
+            }
+            return *this;
+        }
 
-		AsyncComponentsMovePointer &operator=(AsyncComponentsMovePointer &&other) noexcept {
-			if (this != &other && ptr_ != other.ptr_) {
-				delete ptr_;
-				ptr_ = other.ptr_;
-				other.ptr_ = nullptr;
-			}
-			return *this;
-		}
+        AsyncComponentsMovePointer &operator=(AsyncComponentsMovePointer &&other) noexcept {
+            if (this != &other && ptr_ != other.ptr_) {
+                delete ptr_;
+                ptr_ = other.ptr_;
+                other.ptr_ = nullptr;
+            }
+            return *this;
+        }
 
-		operator bool() const {
-			return ptr_;
-		}
+        operator bool() const {
+            return ptr_;
+        }
 
-		AsyncComponents &operator*() const {
-			return *ptr_;
-		}
+        AsyncComponents &operator*() const {
+            return *ptr_;
+        }
 
-		AsyncComponents *operator->() const {
-			return ptr_;
-		}
+        AsyncComponents *operator->() const {
+            return ptr_;
+        }
 
-	private:
-		AsyncComponentsMovePointer &mute(const AsyncComponentsMovePointer &other) {
-			return const_cast<AsyncComponentsMovePointer &>(other);
-		}
+    private:
+        AsyncComponentsMovePointer &mute(const AsyncComponentsMovePointer &other) {
+            return const_cast<AsyncComponentsMovePointer &>(other);
+        }
 
-	private:
-		AsyncComponents *ptr_ = nullptr;
-	};
+    private:
+        AsyncComponents *ptr_ = nullptr;
+    };
 
 public:
 
-	using Pointer = std::shared_ptr<DocumentImpl>;
+    using Pointer = std::shared_ptr<DocumentImpl>;
 
-	DocumentImpl(const fs::path &file, Worker &ownerThread);
+    DocumentImpl(const fs::path &file, Worker &ownerThread);
 
-	~DocumentImpl();
+    ~DocumentImpl();
 
-	void start();
+    void start();
 
-	void bind(DocumentListener &listener);
+    void bind(DocumentListener &listener);
 
-	void unbind();
+    void unbind();
 
-	Charset charset() const;
+    Charset charset() const;
 
-	bool loaded() const;
+    bool loaded() const;
 
-	RowN loadedRowCount() const;
-
-private:
-	void asyncLoadDocument();
-
-	void loadPart(AsyncComponents &comps, const MemBuff &data) const;
-
-	void loadDocument(AsyncComponents &comps) const;
+    RowN loadedRowCount() const;
 
 private:
-	const fs::path path_;
-	AsyncComponentsMovePointer asyncComponents_;
-	Db db_;
-	Worker &ownerThread_;
-	DocumentListener *listener_ = nullptr;
+    void asyncLoadDocument();
+
+    void loadPart(AsyncComponents &comps, const MemBuff &data) const;
+
+    void loadDocument(AsyncComponents &comps) const;
 
 private:
-	Charset charset_ = Charset::Unknown;
-	bool loaded_ = false;
-	RowN loadedRowCount_ = 0;
+    const fs::path path_;
+    AsyncComponentsMovePointer asyncComponents_;
+    Db db_;
+    Worker &ownerThread_;
+    DocumentListener *listener_ = nullptr;
+
+private:
+    Charset charset_ = Charset::Unknown;
+    bool loaded_ = false;
+    RowN loadedRowCount_ = 0;
 };
 
 
