37bc97ca27a8b697eda0f716b874119384cce4b9
蔡心宇 <haocxy@qq.com>
2021-07-18 00:26:21 +0800

    format file

--- src/doc/document.h
+++ src/doc/document.h
@@ -10,49 +10,49 @@ namespace doc
 
 class Document {
 public:
-	Document(const fs::path &file, Worker &ownerThread)
-		: ptr_(std::make_shared<detail::DocumentImpl>(file, ownerThread)) {
+    Document(const fs::path &file, Worker &ownerThread)
+        : ptr_(std::make_shared<detail::DocumentImpl>(file, ownerThread)) {
 
-		// 只有ptr_初始化完成后才能启动需要使用shared_from_this的逻辑，
-		// 因为需要确保共享指针的内部状态正确初始化
-		ptr_->start();
-	}
+        // 只有ptr_初始化完成后才能启动需要使用shared_from_this的逻辑，
+        // 因为需要确保共享指针的内部状态正确初始化
+        ptr_->start();
+    }
 
-	~Document() {
-		assert(ptr_);
-		ptr_->unbind();
+    ~Document() {
+        assert(ptr_);
+        ptr_->unbind();
 
-		// 这一句很重要，这不是普通的指针
-		ptr_ = nullptr;
-	}
+        // 这一句很重要，这不是普通的指针
+        ptr_ = nullptr;
+    }
 
-	void bind(DocumentListener &listener) {
-		assert(ptr_);
-		ptr_->bind(listener);
-	}
+    void bind(DocumentListener &listener) {
+        assert(ptr_);
+        ptr_->bind(listener);
+    }
 
-	void unbind() {
-		assert(ptr_);
-		ptr_->unbind();
-	}
+    void unbind() {
+        assert(ptr_);
+        ptr_->unbind();
+    }
 
-	Charset charset() const {
-		assert(ptr_);
-		return ptr_->charset();
-	}
+    Charset charset() const {
+        assert(ptr_);
+        return ptr_->charset();
+    }
 
-	bool loaded() const {
-		assert(ptr_);
-		return ptr_->loaded();
-	}
+    bool loaded() const {
+        assert(ptr_);
+        return ptr_->loaded();
+    }
 
-	RowN loadedRowCount() const {
-		assert(ptr_);
-		return ptr_->loadedRowCount();
-	}
+    RowN loadedRowCount() const {
+        assert(ptr_);
+        return ptr_->loadedRowCount();
+    }
 
 private:
-	std::shared_ptr<detail::DocumentImpl> ptr_;
+    std::shared_ptr<detail::DocumentImpl> ptr_;
 };
 
 }
