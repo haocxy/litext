341ee6e25ce5c0f779e0a301f8749066f4034bbf
蔡心宇 <haocxy@qq.com>
2021-07-18 00:21:30 +0800

    a

--- src/core/readable_size_util.h
+++ src/core/readable_size_util.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <string>
 
 
@@ -11,59 +12,60 @@ namespace detail
 {
 
 template <typename N, typename U>
-inline std::string convert(N n, U unit, int prec, char suffix) {
-	std::ostringstream ss;
-	if (prec > 0) {
-		const double f = static_cast<double>(n) / static_cast<double>(unit);
-		ss << f;
-		std::string result = ss.str();
-		const int len = static_cast<int>(result.length());
-		for (int i = 0; i < len; ++i) {
-			if (result[i] == '.') {
-				const int resultDigitLen = i + 1 + prec;
-				if (len > resultDigitLen) {
-					result.resize(resultDigitLen + 1);
-					result[resultDigitLen] = suffix;
-					return result;
-				} else {
-					const int d = resultDigitLen - len;
-					for (int j = 0; j < d; ++j) {
-						result.push_back('0');
-					}
-					result.push_back(suffix);
-					return result;
-				}
-			}
-		}
-		result.push_back('.');
-		for (int i = 0; i < prec; ++i) {
-			result.push_back('0');
-		}
-		result.push_back(suffix);
-		return result;
-	} else {
-		ss << (n / unit) << suffix;
-		return ss.str();
-	}
+inline std::string convert(N n, U unit, int prec, const char *suffix) {
+    assert(suffix);
+    std::ostringstream ss;
+    if (prec > 0) {
+        const double f = static_cast<double>(n) / static_cast<double>(unit);
+        ss << f;
+        std::string result = ss.str();
+        const int len = static_cast<int>(result.length());
+        for (int i = 0; i < len; ++i) {
+            if (result[i] == '.') {
+                const int resultDigitLen = i + 1 + prec;
+                if (len > resultDigitLen) {
+                    result.resize(resultDigitLen);
+                    result.append(suffix);
+                    return result;
+                } else {
+                    const int d = resultDigitLen - len;
+                    for (int j = 0; j < d; ++j) {
+                        result.push_back('0');
+                    }
+                    result.append(suffix);
+                    return result;
+                }
+            }
+        }
+        result.push_back('.');
+        for (int i = 0; i < prec; ++i) {
+            result.push_back('0');
+        }
+        result.append(suffix);
+        return result;
+    } else {
+        ss << (n / unit) << suffix;
+        return ss.str();
+    }
 }
 
 }
 
 template <typename N>
 inline std::string convert(N n, int prec = 1) {
-	constexpr N kb = 1024;
-	constexpr N mb = kb * 1024;
-	constexpr N gb = mb * 1024;
-	if (n > gb) {
-		return detail::convert(n, gb, prec, 'g');
-	}
-	if (n > mb) {
-		return detail::convert(n, mb, prec, 'm');
-	}
-	if (n > kb) {
-		return detail::convert(n, kb, prec, 'k');
-	}
-	return detail::convert(n, 1, 0, 'B');
+    constexpr N kb = 1024;
+    constexpr N mb = kb * 1024;
+    constexpr N gb = mb * 1024;
+    if (n > gb) {
+        return detail::convert(n, gb, prec, "GB");
+    }
+    if (n > mb) {
+        return detail::convert(n, mb, prec, "MB");
+    }
+    if (n > kb) {
+        return detail::convert(n, kb, prec, "KB");
+    }
+    return detail::convert(n, 1, 0, "B");
 }
 
 }
