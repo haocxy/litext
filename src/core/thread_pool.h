b1d201ad706d5fa449ffef4ae61f5c425d5a93e5
蔡心宇 <haocxy@qq.com>
2021-07-19 01:12:31 +0800

    恢复之前删掉的文件

---
+++ src/core/thread_pool.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <atomic>
+#include <vector>
+#include <thread>
+#include <functional>
+
+#include "worker.h"
+#include "block_queue.h"
+
+
+class ThreadPool : public Worker {
+public:
+	ThreadPool(size_t maxThreadCount);
+
+	virtual ~ThreadPool();
+
+	virtual void post(std::function<void()> &&work) override;
+
+private:
+	void threadLoop();
+
+private:
+	using Work = std::function<void()>;
+	std::atomic_bool stopped_ = false;
+	const size_t maxThreadCount_;
+	std::vector<std::thread> threads_;
+	BlockQueue<Work> workQueue_;
+};
