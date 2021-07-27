409c45b7d24d963173fb435fbc3c885a1c50c28f
蔡心宇 <haocxy@qq.com>
2021-07-27 15:31:42 +0800

    字符宽度缓存的内部容器由map改为unordered_map

--- src/gui/cached_char_pix_width_provider.h
+++ src/gui/cached_char_pix_width_provider.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <unordered_map>
 
 #include "core/font_index.h"
 #include "core/font.h"
@@ -23,7 +24,7 @@ private:
     font::FontContext context_;
     font::FontFile fontFile_;
     font::FontFace fontFace_;
-    std::map<char32_t, int> cacheUnicodeToPixWidth_;
+    std::unordered_map<char32_t, int> cacheUnicodeToPixWidth_{ 65536 };
 };
 
 }
