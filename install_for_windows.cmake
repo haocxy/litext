42483f33b600bdd2a72c424f8ae1f25505fa4e48
蔡心宇 <haocxy@qq.com>
2021-07-28 17:49:06 +0800

    把windows平台的安装配置移到install_for_windows.cmake

---
+++ install_for_windows.cmake
@@ -0,0 +1,44 @@
+file(GLOB_RECURSE asset_files "assets/*")
+install(FILES ${asset_files} DESTINATION assets)
+
+install(TARGETS notesharp RUNTIME DESTINATION bin CONFIGURATIONS Release)
+
+find_file(dlib_qt5core "Qt5Core.dll")
+install(FILES ${dlib_qt5core} DESTINATION bin)
+
+find_file(dlib_qt5gui "Qt5Gui.dll")
+install(FILES ${dlib_qt5gui} DESTINATION bin)
+
+find_file(dlib_qt5widgets "Qt5Widgets.dll")
+install(FILES ${dlib_qt5widgets} DESTINATION bin)
+
+find_file(dlib_zlibd "zlibd.dll")
+install(FILES ${dlib_zlibd} DESTINATION bin)
+
+find_file(dlib_png "libpng16d.dll")
+install(FILES ${dlib_png} DESTINATION bin)
+
+find_file(dlib_msvcp140 "MSVCP140.dll")
+install(FILES ${dlib_msvcp140} DESTINATION bin)
+
+find_file(dlib_vcruntime140 "VCRUNTIME140.dll")
+install(FILES ${dlib_vcruntime140} DESTINATION bin)
+
+find_file(dlib_vcruntime140_1 "VCRUNTIME140_1.dll")
+install(FILES ${dlib_vcruntime140_1} DESTINATION bin)
+
+find_file(dlib_vcruntime140d "VCRUNTIME140D.dll")
+install(FILES ${dlib_vcruntime140d} DESTINATION bin)
+
+find_file(dlib_ucrtbased "ucrtbased.dll")
+install(FILES ${dlib_ucrtbased} DESTINATION bin)
+
+find_file(dlib_qtplugin_platforms_windows "qwindows.dll" PATH_SUFFIXES plugins/platforms)
+install(FILES ${dlib_qtplugin_platforms_windows} DESTINATION bin/platforms)
+
+find_file(dlib_qtplugin_styles_vistastyle "qwindowsvistastyle.dll" PATH_SUFFIXES plugins/styles)
+install(FILES ${dlib_qtplugin_styles_vistastyle} DESTINATION bin/styles)
+
+set(CPACK_GENERATOR ZIP)
+set(CPACK_PACKAGE_NAME "NoteSharp")
+include(CPack)
