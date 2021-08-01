3c16650d5d3050a9d20b08f86f7d3e53e191fcef
蔡心宇 <haocxy@qq.com>
2021-08-01 15:27:12 +0800

    添加Range类，简化范围的表示

---
+++ src/core/range.h
@@ -0,0 +1,52 @@
+#pragma once
+
+
+template <typename T>
+class Range {
+public:
+    Range() {}
+
+    static Range byOffAndLen(T off, T len) {
+        return Range(off, len);
+    }
+
+    static Range byBegAndEnd(T beg, T end) {
+        return Range(beg, end - beg);
+    }
+
+    static Range byLeftAndRight(T left, T right) {
+        return Range(left, right - left + 1);
+    }
+
+    T off() const {
+        return beg_;
+    }
+
+    T len() const {
+        return len_;
+    }
+
+    T beg() const {
+        return beg_;
+    }
+
+    T end() const {
+        return beg_ + len_;
+    }
+
+    T left() const {
+        return beg_;
+    }
+
+    T right() const {
+        return end() - 1;
+    }
+
+private:
+    Range(T beg, T len)
+        : beg_(beg), len_(len) {}
+
+private:
+    T beg_ = T();
+    T len_ = T();
+};
