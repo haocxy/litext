b919b8cdad368fc549a5bff5d7356d2573f95de9
蔡心宇 <haocxy@qq.com>
2021-08-20 13:08:14 +0800

    DocPart

--- src/doc/doc_part.h
+++ src/doc/doc_part.h
@@ -2,15 +2,42 @@
 
 #include "doc_define.h"
 
+
 namespace doc
 {
 
 class DocPart {
 public:
+    DocPart() {}
+
+    PartId id() const {
+        return id_;
+    }
+
+    void setId(PartId id) {
+        id_ = id;
+    }
+
+    const RowRange &rowRange() const {
+        return rowRange_;
+    }
+
+    void setRowRange(const RowRange &rowRange) {
+        rowRange_ = rowRange;
+    }
+
+    const ByteRange &byteRange() const {
+        return byteRange_;
+    }
+
+    void setByteRange(const ByteRange &byteRange) {
+        byteRange_ = byteRange;
+    }
 
 private:
     PartId id_{ 0 };
-
+    RowRange rowRange_;
+    ByteRange byteRange_;
 };
 
 }
