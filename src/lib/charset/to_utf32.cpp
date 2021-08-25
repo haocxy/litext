c38b1be728bf6372ed3f7cfb25de75549cad4dad
蔡心宇 <haocxy@qq.com>
2021-08-26 01:51:52 +0800

    补充Qt库不支持ASCII解码器

--- src/lib/charset/to_utf32.cpp
+++ src/lib/charset/to_utf32.cpp
@@ -11,8 +11,23 @@
 namespace charset
 {
 
+static std::u32string toUTF32ForAscii(const void *data, i64 nbytes)
+{
+    std::u32string s(nbytes, 0);
+    const char *str = reinterpret_cast<const char *>(data);
+    for (i64 i = 0; i < nbytes; ++i) {
+        s.push_back(str[i]);
+    }
+    return s;
+}
+
 std::u32string toUTF32(Charset srcCharset, const void *data, i64 nbytes)
 {
+    // Qt库没有提供ASCII的解码器，所以使用Qt库解码时需要单独处理ASCII
+    if (srcCharset == Charset::Ascii) {
+        return toUTF32ForAscii(data, nbytes);
+    }
+
     QTextCodec *codec = QTextCodec::codecForName(CharsetUtil::charsetToStr(srcCharset));
     if (!codec) {
         std::ostringstream ss;
