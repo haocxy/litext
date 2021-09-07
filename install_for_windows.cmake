c6482e413665c38729463161ad4383ab6678aeb9
蔡心宇 <haocxy@qq.com>
2021-09-07 17:28:20 +0800

    windows平台打包时程序文件放在打包目录的根目录

--- install_for_windows.cmake
+++ install_for_windows.cmake
@@ -1,45 +1,45 @@
 file(GLOB_RECURSE asset_files "assets/*")
-install(FILES ${asset_files} DESTINATION assets)
+install(FILES ${asset_files} DESTINATION "assets")
 
 install(TARGETS litext litextcmd litext_body
     CONFIGURATIONS Release
-    RUNTIME DESTINATION bin)
+    RUNTIME DESTINATION ".")
 
 find_file(dlib_qt5core "Qt5Core.dll")
-install(FILES ${dlib_qt5core} DESTINATION bin)
+install(FILES ${dlib_qt5core} DESTINATION ".")
 
 find_file(dlib_qt5gui "Qt5Gui.dll")
-install(FILES ${dlib_qt5gui} DESTINATION bin)
+install(FILES ${dlib_qt5gui} DESTINATION ".")
 
 find_file(dlib_qt5widgets "Qt5Widgets.dll")
-install(FILES ${dlib_qt5widgets} DESTINATION bin)
+install(FILES ${dlib_qt5widgets} DESTINATION ".")
 
 find_file(dlib_zlibd "zlibd.dll")
-install(FILES ${dlib_zlibd} DESTINATION bin)
+install(FILES ${dlib_zlibd} DESTINATION ".")
 
 find_file(dlib_png "libpng16d.dll")
-install(FILES ${dlib_png} DESTINATION bin)
+install(FILES ${dlib_png} DESTINATION ".")
 
 find_file(dlib_msvcp140 "MSVCP140.dll")
-install(FILES ${dlib_msvcp140} DESTINATION bin)
+install(FILES ${dlib_msvcp140} DESTINATION ".")
 
 find_file(dlib_vcruntime140 "VCRUNTIME140.dll")
-install(FILES ${dlib_vcruntime140} DESTINATION bin)
+install(FILES ${dlib_vcruntime140} DESTINATION ".")
 
 find_file(dlib_vcruntime140_1 "VCRUNTIME140_1.dll")
-install(FILES ${dlib_vcruntime140_1} DESTINATION bin)
+install(FILES ${dlib_vcruntime140_1} DESTINATION ".")
 
 find_file(dlib_vcruntime140d "VCRUNTIME140D.dll")
-install(FILES ${dlib_vcruntime140d} DESTINATION bin)
+install(FILES ${dlib_vcruntime140d} DESTINATION ".")
 
 find_file(dlib_ucrtbased "ucrtbased.dll")
-install(FILES ${dlib_ucrtbased} DESTINATION bin)
+install(FILES ${dlib_ucrtbased} DESTINATION ".")
 
 find_file(dlib_qtplugin_platforms_windows "qwindows.dll" PATH_SUFFIXES plugins/platforms)
-install(FILES ${dlib_qtplugin_platforms_windows} DESTINATION bin/platforms)
+install(FILES ${dlib_qtplugin_platforms_windows} DESTINATION "platforms")
 
 find_file(dlib_qtplugin_styles_vistastyle "qwindowsvistastyle.dll" PATH_SUFFIXES plugins/styles)
-install(FILES ${dlib_qtplugin_styles_vistastyle} DESTINATION bin/styles)
+install(FILES ${dlib_qtplugin_styles_vistastyle} DESTINATION "styles")
 
 set(CPACK_GENERATOR ZIP)
 set(CPACK_PACKAGE_NAME "litext")
