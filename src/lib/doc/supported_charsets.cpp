ca04b6e9ac95a14aedb79b5429ad16fb4ecd3b19
蔡心宇 <haocxy@qq.com>
2021-09-19 12:16:04 +0800

    统一定义程序支持的字符集

---
+++ src/lib/doc/supported_charsets.cpp
@@ -0,0 +1,13 @@
+#include "supported_charsets.h"
+
+
+namespace doc
+{
+
+const std::set<Charset> gSupportedCharsets{
+    Charset::Ascii,
+    Charset::UTF_8,
+    Charset::GB18030,
+};
+
+}
---
+++ src/lib/doc/supported_charsets.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <set>
+
+#include "core/charset.h"
+
+
+namespace doc
+{
+
+extern const std::set<Charset> gSupportedCharsets;
+
+}
