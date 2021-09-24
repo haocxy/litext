c18aaf52c54eb15e73cd2c3747753bcc9895548c
蔡心宇 <haocxy@qq.com>
2021-09-24 19:38:23 +0800

    添加汉语翻译文件

---
+++ trans/trans.zh_CN.ts
@@ -0,0 +1,375 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>BigFileGenerator</name>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="14"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="215"/>
+        <source>BigFileGenerator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="23"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="216"/>
+        <source>Output File Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="32"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="217"/>
+        <source>choose</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="44"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="218"/>
+        <source>Output File Charset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="75"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="220"/>
+        <source>Output File Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="87"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="221"/>
+        <source>1.3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="97"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="222"/>
+        <source>GB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="107"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="223"/>
+        <source>MB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="120"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="224"/>
+        <source>Row Size Range</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="126"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="225"/>
+        <source>Min:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="133"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="226"/>
+        <source>30</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="140"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="227"/>
+        <source>Max:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="147"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="228"/>
+        <source>60</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="154"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="229"/>
+        <source>(Unit: Char Count)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="194"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="230"/>
+        <source>open after generated</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator.ui" line="221"/>
+        <location filename="../src/lib/qt/gen.big_file_generator.ui.h" line="231"/>
+        <source>execute</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EditorStatusBar</name>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="14"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="150"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="28"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="151"/>
+        <source>File Size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="35"/>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="60"/>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="85"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="152"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="154"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="156"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="53"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="153"/>
+        <source>Row Count:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="78"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="155"/>
+        <source>Charset:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar.ui" line="116"/>
+        <location filename="../src/lib/qt/gen.editor_status_bar.ui.h" line="158"/>
+        <source>Loading</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gui::qt::BigFileGeneratorWidget</name>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="168"/>
+        <source>Choose Output File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="238"/>
+        <source>
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="240"/>
+        <source>Warning! </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="246"/>
+        <source>Your confirm is needed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="254"/>
+        <source>Please specify an output file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="260"/>
+        <source>Please use absolute path for your file&apos;s safe.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="269"/>
+        <source>Path of output file [%1] is a directory.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="274"/>
+        <source>Output file [ %1 ] is not a regular file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="278"/>
+        <source>Output file [ %1 ] already existed.
+It&apos;s content will be deleted if you confirm.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="294"/>
+        <source>Please specify the size of output file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="302"/>
+        <source>Please check only one unit: [GB] or [MB].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="307"/>
+        <source>Please check unit.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="330"/>
+        <source>Please specify minimum value for row size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="336"/>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="348"/>
+        <source>Values of row size should be at least %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="342"/>
+        <source>Please specify maximum value for row size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="353"/>
+        <source>Minimum value of row size should not be greater than maximum value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="389"/>
+        <source>Cannot open file [ %1 ] for write.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="397"/>
+        <source>Cannot make codec for charset [ %1 ].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="403"/>
+        <source>Cannot make encoder for charset [ %1 ].</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="442"/>
+        <source>Cannot make codec for UTF-8</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/big_file_generator_widget.cpp" line="448"/>
+        <source>Cannot make decoder for UTF-8</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gui::qt::EditorStatusBarWidget</name>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar_widget.cpp" line="117"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_status_bar_widget.cpp" line="143"/>
+        <source>Loaded by %1 ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gui::qt::EditorWidget</name>
+    <message>
+        <location filename="../src/lib/qt/editor_widget.cpp" line="91"/>
+        <source>row too big</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_widget.cpp" line="93"/>
+        <source>unsupported charset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/editor_widget.cpp" line="95"/>
+        <source>unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gui::qt::MainWindow</name>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="223"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="224"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished">打开</translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="225"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="227"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="228"/>
+        <source>&amp;Jump</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="230"/>
+        <source>&amp;TestTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="231"/>
+        <source>&amp;MakeBigFile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="236"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="237"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="239"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="240"/>
+        <location filename="../src/lib/qt/main_window.cpp" line="266"/>
+        <source>Jump</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="243"/>
+        <source>MakeBigFile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="266"/>
+        <source>Jump to line in doument</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/main_window.cpp" line="337"/>
+        <source>Open Document</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gui::qt::TrayMenu</name>
+    <message>
+        <location filename="../src/lib/qt/tray_menu.cpp" line="11"/>
+        <source>Activate UI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/lib/qt/tray_menu.cpp" line="14"/>
+        <source>Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
