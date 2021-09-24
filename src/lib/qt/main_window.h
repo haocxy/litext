7cc54821119ab97a740b64875d831596d3992575
蔡心宇 <haocxy@qq.com>
2021-09-24 18:04:22 +0800

    对Action函数的命名添加注释

--- src/lib/qt/main_window.h
+++ src/lib/qt/main_window.h
@@ -47,12 +47,23 @@ private:
         connect(action, &QAction::triggered, this, f);
     }
 
-private:
+private: // Action 统一放在这里
+
+    // 这些由某个按钮或菜单触发的操作统一以 Action 结尾
+    // 这样做是为了与那些实际执行逻辑的函数区分
+    // 
+    // 例如对于 fileOpenAction() , 如果只写成 fileOpen()
+    // 就会让人产生 "为什么没有用参数指明要打开哪个文件" 之类的疑问
+    //
+    // 以 Action 为后缀, 就表明这只是一个 GUI 动作, 并不执行实际的逻辑
+
     void fileOpenAction();
 
     void viewJumpAction();
 
     void makeBigFileAction();
+
+private:
 
     void curEditorPageUp();
 
